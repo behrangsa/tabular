0ebd4107be84c7da5fa01a5157a755f24e44026a
Amadeus <behrangsa@gmail.com>
2025-04-12 10:56:40 +0330

    (feat) update background script to close non-pinned, non-extension tabs on action click

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,5 +1,38 @@
-// Background script for the Tabular extension
-chrome.action.onClicked.addListener(() => {
-  // Open the tab manager page in a new tab when the extension icon is clicked
-  chrome.tabs.create({ url: chrome.runtime.getURL("assets/tab-page.html") });
-});
+/*
+ * Background script for the Tabular MV3 extension.
+ */
+
+const isCurrentExtensionTab = (tab: chrome.tabs.Tab): boolean => {
+  const extensionBaseUrl = chrome.runtime.getURL('');
+
+  return !!tab.url && tab.url.startsWith(extensionBaseUrl);
+}
+
+const isPinnedTab = (tab: chrome.tabs.Tab): boolean => {
+  // Check if the tab is pinned
+  return !!tab.pinned;
+}
+
+const isCloseableTab = (tab: chrome.tabs.Tab): boolean => {
+  // Check if the tab is closeable
+  if (tab.id) {
+    return !isCurrentExtensionTab(tab) && !isPinnedTab(tab);
+  } else {
+    return false;
+  }
+}
+
+const handleActionClicked = () => {
+  chrome.tabs.query({}, (tabs) => {
+    const now = new Date();
+    const closeableTabs = tabs.filter(isCloseableTab)
+
+    closeableTabs.forEach((tab) => {
+      chrome.tabs.remove(tab.id!);
+    });
+  });
+}
+
+// Add an onClicked listener
+chrome.action.onClicked.addListener(handleActionClicked);
+
